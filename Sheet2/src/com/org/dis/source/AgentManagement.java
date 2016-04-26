98eb10d7c0885061f19b9baf0667e0e82debf3bb
Nikhil Churamani <nikhil.churamani@yahoo.com>
2016-04-26 21:17:59 +0200

    UPDATE AGENT MODULE, LOGIN AND WRAPPER

--- Sheet2/src/com/org/dis/source/AgentManagement.java
+++ Sheet2/src/com/org/dis/source/AgentManagement.java
@@ -24,20 +24,25 @@ public class AgentManagement {
 	private String address;
 	private String login;
 	private String password;
+	private static String loggedinUser;
 	
-	public AgentManagement() {
+	public AgentManagement(String loggedin) {
 		// TODO Auto-generated constructor stub
+		AgentManagement.loggedinUser = loggedin;
+		
 	}
 
 	public void agentMenu(){
 		char choice = '0';
 		System.out.println("-------------------------------------------------");
 		System.out.println("------------AGENT MANAGEMENT MODULE--------------");
-		System.out.println("------------WELCOME " + this.getLogin() + "------------------------");
+		System.out.println("------------WELCOME " + this.getloggedinUser() + "------------------------");
 		System.out.println("-------------------------------------------------");
 		System.out.println("Press 1 for Agent ID Creation");
 		System.out.println("Press 2 for Update Agent Details");
-		System.out.println("Press 3 for Delete and Agent");
+		System.out.println("Press 3 for Delete an Agent");
+		System.out.println("Press 4 to go back to Main Menu");
+
 		try{
 			 choice = (char) new BufferedReader(new InputStreamReader(System.in)).read();
 		}catch(Exception e){
@@ -45,7 +50,7 @@ public class AgentManagement {
 		}
 		if (choice == '1'){
 			System.out.println("-------------------------------------------------");
-			System.out.println("------------WELCOME " + this.getLogin() + "------------------------");
+			System.out.println("------------WELCOME " + this.getloggedinUser() + "------------------------");
 			System.out.println("------CREATE AGENT MANAGEMENT MODULE--------------");
 			System.out.println("-------------------------------------------------");
 			InputStreamReader ir = new InputStreamReader(System.in);
@@ -65,36 +70,96 @@ public class AgentManagement {
 			createAgent();
 		}
 		else if(choice=='2'){
-		/*	System.out.println("-------------------------------------------------");
-			System.out.println("------------WELCOME " + this.getLogin() + "------------------------");
+			char choiceupdate = '0';
+			System.out.println("-------------------------------------------------");
+			System.out.println("------------WELCOME " + this.getloggedinUser() + "------------------------");
 			System.out.println("-----UPDATE AGENT MANAGEMENT MODULE--------------");
 			System.out.println("-------------------------------------------------");
 			InputStreamReader ir = new InputStreamReader(System.in);
 			BufferedReader br = new BufferedReader(ir);
 			
 			try{
-				System.out.println("Enter AGENT NAME:");
-				this.setName(br.readLine());
-				System.out.println("Enter AGENT ADDRESS:");
-				this.setAddress(br.readLine());
-				System.out.println("Enter AGENT LOGIN:");
+				System.out.println("Enter AGENT LOGIN TO VIEW DETAILS:");
 				this.setLogin(br.readLine());
-				System.out.println("Enter AGENT PASSWORD:");
-				this.setPassword(br.readLine());
+				
+				readAgent();
+				
+				System.out.println("Name: "+ this.getName());
+				System.out.println("Address: " + this.getAddress());
+				System.out.println("Login: " + this.getLogin());
+				System.out.println("Password: "+ this.getPassword());
+				System.out.println("-------------------------------------------------");
+				System.out.println("Press 1 for Edit Name");
+				System.out.println("Press 2 for Edit Address");
+				System.out.println("Press 3 for Edit Password");
+				System.out.println("Note: Cannot edit LOGIN. Please contact DB Admin.");
+				choiceupdate = (char) new BufferedReader(new InputStreamReader(System.in)).read();
+				
+				if(choiceupdate=='1'){
+					System.out.println("Enter New Name: ");
+					this.setName(br.readLine());
+				}
+				else if(choiceupdate =='2'){
+					System.out.println("Enter New Address: ");
+					this.setAddress(br.readLine());
+				}
+				else if(choiceupdate =='3'){
+					System.out.println("Enter New Password: ");
+					this.setPassword(br.readLine());
+				}
+				else{
+					System.out.println("Invalid Choice! Try again!");
+					agentMenu();
+				}
+				updateAgent();			
 			}
 			catch(IOException io){
 				io.printStackTrace();
 			}
-			createAgent();		
-		*/
-		System.out.println("Call Update");
+			System.out.println("Agent details updated!");
+			agentMenu();
 		}
 
 		else if(choice=='3'){
-			System.out.println("Call Delete");
+			System.out.println("-------------------------------------------------");
+			System.out.println("------------WELCOME " + this.getloggedinUser() + "------------------------");
+			System.out.println("-----DELETE AGENT MANAGEMENT MODULE--------------");
+			System.out.println("-------------------------------------------------");
+			InputStreamReader ir = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(ir);
+			
+			try{
+				System.out.println("Enter AGENT LOGIN TO DELETE:");
+				this.setLogin(br.readLine());
+				
+				readAgent();
+				
+				System.out.println("CONFIRM AGENT DELETE (Y/N):");
+				char choicedelete = (char) new BufferedReader(new InputStreamReader(System.in)).read();
+				if(choicedelete == 'Y'){
+					deleteAgent();
+				}
+				else if(choicedelete=='N'){
+					System.out.println("Agent not deleted!");
+					agentMenu();
+				}
+				else{
+					System.out.println("Invalid Choice! Try Again!");
+					agentMenu();
+				}
+			}
+			catch(IOException io){
+				io.printStackTrace();
+			}
+			System.out.println("Agent" + this.getLogin() + "deleted!");
+			agentMenu();
+		}
+		else if(choice=='4'){
+			Main.menuDisplay(this.getloggedinUser());
 		}
 		else{
 			System.out.println("Invalid choice! Chose again!");
+			agentMenu();
 		}
 	}
 	
@@ -121,6 +186,12 @@ public class AgentManagement {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public String getloggedinUser() {
+		return loggedinUser;
+	}
+	public void setloggedinUser(String loggedinUser) {
+		AgentManagement.loggedinUser = loggedinUser;
 	}
 	
 	public void createAgent(){
@@ -158,8 +229,8 @@ public class AgentManagement {
 		String loginid = null;
 		try{
 			/**
-			 * Login credentials are checked in the Estate_Agent table and the 
-			 * corresponding ID is returned.
+			 * Autogenerate is used to get the max login from Estate_Agent and then increment it by 1. Also it generates
+			 * password for the user.
 			 */
 			String sql;
 			sql = "SELECT MAX(LOGIN) FROM ESTATE_AGENT";
@@ -184,5 +255,81 @@ public class AgentManagement {
 		loginid = alphanumeric[0].concat(String.format("%03d", numeric));
 		this.setLogin(loginid);
 		this.setPassword("AGENT".concat(loginid));
+	}
+	
+	public void readAgent(){
+		DBConManager con = new DBConManager();
+		con.CreateConnection();
+		try{
+			/**
+			 * Read agent is used read the details of the user requested. 
+			 */
+			String sql;
+			sql = "SELECT NAME, ADDR, LOGIN, PASSWORD FROM ESTATE_AGENT WHERE LOGIN =?";
+			PreparedStatement pstm = con.getConnection().prepareStatement(sql);
+			pstm.setString(1, this.getLogin());
+			
+			ResultSet rs = pstm.executeQuery();
+			if(!rs.next()){
+				System.out.println("Invalid Login! Try again!");
+				agentMenu();
+			}
+			while(rs.next()){
+				System.out.println(rs.getString(1));
+				this.setName(rs.getString(1));
+				this.setAddress(rs.getString(2));
+				this.setLogin(rs.getString(3));
+				this.setPassword(rs.getString(4));
+			}
+			rs.close();
+			pstm.close();
+			con.closeConnection();;
+		}
+		catch(Exception e){
+			e.printStackTrace();	
+		}
+	}
+	public void updateAgent(){
+		DBConManager con = new DBConManager();
+		con.CreateConnection();
+		try{
+			/**
+			 * Read agent is used read the details of the user requested. 
+			 */
+			String sql;
+			sql = "UPDATE ESTATE_AGENT SET NAME=?, ADDR=?, PASSWORD=? WHERE LOGIN=?";
+			PreparedStatement pstm = con.getConnection().prepareStatement(sql);
+			pstm.setString(1, this.getName());
+			pstm.setString(2, this.getAddress());
+			pstm.setString(3, this.getPassword());
+			pstm.setString(4, this.getLogin());
+			
+			pstm.executeUpdate();
+			pstm.close();
+			con.closeConnection();;
+		}
+		catch(Exception e){
+			e.printStackTrace();	
+		}
+	}
+	public void deleteAgent(){
+		DBConManager con = new DBConManager();
+		con.CreateConnection();
+		try{
+			/**
+			 * Read agent is used read the details of the user requested. 
+			 */
+			String sql;
+			sql = "DELETE FROM ESTATE_AGENT WHERE LOGIN = ? ";
+			PreparedStatement pstm = con.getConnection().prepareStatement(sql);
+			pstm.setString(1, this.getLogin());
+			
+			pstm.executeUpdate();
+			pstm.close();
+			con.closeConnection();;
+		}
+		catch(Exception e){
+			e.printStackTrace();	
+		}
 	}
 }
